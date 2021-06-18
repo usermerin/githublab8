f4bde4f31e12c9a407791d1db73ae5e433bc1a81
Merin John <merinjohnanchery@gmail.com>
2021-06-17 20:49:21 -0400

    Initial commit

---
+++ Source.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "lab8-1.h" // Insert contents of the header file at this point. Note use of double quotes here.
+int main()
+{
+	std::cout << "The sum of 3 and 4 is " << add(3, 4) << '\n';
+	return 0;
+}
